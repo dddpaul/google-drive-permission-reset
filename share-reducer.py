37c97f74e93d1f461b67c7209268ccbb50cba9d1
Pavel Derendyaev <dddpaul@gmail.com>
2023-05-31 21:33:42 +0300

    Initial commit

---
+++ share-reducer.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.errors import HttpError
+
+# If modifying these SCOPES, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def main():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+
+    # Call the Drive v3 API
+    results = service.files().list(
+        pageSize=1000, fields="nextPageToken, files(id, name, permissions)").execute()
+    items = results.get('files', [])
+    
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+            permissions = item.get('permissions', [])
+            for perm in permissions:
+                if perm['type'] == 'anyone':
+                    try:
+                        service.permissions().delete(
+                            fileId=item['id'],
+                            permissionId=perm['id']
+                        ).execute()
+                        print('Changed sharing settings for file: %s' % item['name'])
+                    except HttpError as error:
+                        print('An error occurred: %s' % error)
+
+if __name__ == '__main__':
+    main()
