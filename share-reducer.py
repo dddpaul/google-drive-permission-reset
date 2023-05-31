d58d3c4c9572ab36209ceac073266c8744c998cc
Pavel Derendyaev <dddpaul@gmail.com>
2023-05-31 23:18:21 +0300

    Add recursive traverse

--- share-reducer.py
+++ share-reducer.py
@@ -11,6 +11,42 @@ from googleapiclient.errors import HttpError
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
 
+def process_files(service, folder_id):
+    # Get files in the folder
+    file_results = service.files().list(
+        q="'{}' in parents".format(folder_id),
+        pageSize=1000, fields="nextPageToken, files(id, name, permissions)").execute()
+    file_items = file_results.get('files', [])
+
+    if not file_items:
+        print('No files found in the folder.')
+    else:
+        print('Files:')
+        for item in file_items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+            permissions = item.get('permissions', [])
+            for perm in permissions:
+                if perm['type'] == 'anyone':
+                    try:
+                        service.permissions().delete(
+                            fileId=item['id'],
+                            permissionId=perm['id']
+                        ).execute()
+                        print('Changed sharing settings for file: %s' % item['name'])
+                    except HttpError as error:
+                        print('An error occurred: %s' % error)
+
+    # Now process subfolders
+    folder_results = service.files().list(
+        q="'{}' in parents and mimeType='application/vnd.google-apps.folder'".format(folder_id),
+        fields="files(id, name)").execute()
+    folder_items = folder_results.get('files', [])
+
+    for item in folder_items:
+        print('Entering subfolder:', item['name'])
+        process_files(service, item['id'])
+
+
 def main():
     # Parsing command line arguments
     parser = argparse.ArgumentParser()
@@ -48,29 +84,7 @@ def main():
 
         folder_id = folder_items[0]['id']
 
-    # Get files in the folder
-    file_results = service.files().list(
-        q="'{}' in parents".format(folder_id),
-        pageSize=1000, fields="nextPageToken, files(id, name, permissions)").execute()
-    file_items = file_results.get('files', [])
-
-    if not file_items:
-        print('No files found in the folder.')
-    else:
-        print('Files:')
-        for item in file_items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
-            permissions = item.get('permissions', [])
-            for perm in permissions:
-                if perm['type'] == 'anyone':
-                    try:
-                        service.permissions().delete(
-                            fileId=item['id'],
-                            permissionId=perm['id']
-                        ).execute()
-                        print('Changed sharing settings for file: %s' % item['name'])
-                    except HttpError as error:
-                        print('An error occurred: %s' % error)
+    process_files(service, folder_id)
 
 
 if __name__ == '__main__':
